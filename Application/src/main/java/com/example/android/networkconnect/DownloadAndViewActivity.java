23d7f2235669967c0de82d5f6e69bd73abd8c8b0
FauxMacBook <you@example.com>
2017-10-25 14:20:08 +0200

    First version.

---
+++ Application/src/main/java/com/example/android/networkconnect/DownloadAndViewActivity.java
@@ -0,0 +1,139 @@
+package com.example.android.networkconnect;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.io.File;
+
+public class DownloadAndViewActivity extends FragmentActivity implements DownloadCallback {
+    private static String DEBUG = "COUCOU";
+
+    // Reference to the TextView showing fetched data, so we can clear it with a button
+    // as necessary.
+    private VideoView mDataView;
+    private String path = "https://ia800201.us.archive.org/22/items/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+    private EditText mDLURLtextField ;
+    private String tempFilePath;
+    // Keep a reference to the NetworkFragment which owns the AsyncTask object
+    // that is used to execute network ops.
+    private NetworkFragment mNetworkFragment;
+
+    // Boolean telling us whether a download is in progress, so we don't trigger overlapping
+    // downloads with consecutive button clicks.
+    private boolean mDownloading = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dl_and_view_main);
+        // Add playback controls.
+        MediaController vidControl = new MediaController(this);
+        // Set it to use the VideoView instance as its anchor.
+        vidControl.setAnchorView(mDataView);
+        // Set it as the media controller for the VideoView object.
+        mDataView= (VideoView) findViewById(R.id.video);
+        mDataView.setMediaController(vidControl);
+
+        mDLURLtextField = (EditText)findViewById(R.id.mediaURL);
+        mDLURLtextField.setText(path);
+//        mDataVideo = (VideoView) findViewById(R.id.video);
+
+        mNetworkFragment = NetworkFragment.getInstance(getSupportFragmentManager(), path);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.dl_and_view_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // When the user clicks FETCH, fetch the first 500 characters of
+            // raw HTML from www.google.com.
+            case R.id.fetch_action:
+                mNetworkFragment.setmUrlString(mDLURLtextField.getText().toString());
+                startDownload();
+                return true;
+            // Clear the text and cancel download.
+            case R.id.clear_action:
+                finishDownloading();
+                //File.delete(tempFilePath);
+                mDLURLtextField.setText("");
+                return true;
+
+            case R.id.back_action:
+                Intent mIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mIntent);
+                return true;
+
+
+        }
+        return false;
+    }
+
+    private void startDownload() {
+        if (!mDownloading && mNetworkFragment != null) {
+            // Execute the async download.
+            mNetworkFragment.startDownload();
+            mDownloading = true;
+        }
+    }
+
+
+    @Override
+    public void updateFromDownload(String result) {
+        if (result != null) {
+            Log.d(DEBUG,result);
+            tempFilePath = result;
+            mDataView.setVideoPath(result);
+            mDataView.start();
+        }
+    }
+
+    @Override
+    public NetworkInfo getActiveNetworkInfo() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo;
+    }
+
+    @Override
+    public void finishDownloading() {
+        mDownloading = false;
+        if (mNetworkFragment != null) {
+            mNetworkFragment.cancelDownload();
+        }
+    }
+
+    @Override
+    public void onProgressUpdate(int progressCode, int percentComplete) {
+        switch(progressCode) {
+            // You can add UI behavior for progress updates here.
+            case Progress.ERROR:
+                break;
+            case Progress.CONNECT_SUCCESS:
+                break;
+            case Progress.GET_INPUT_STREAM_SUCCESS:
+                break;
+            case Progress.PROCESS_INPUT_STREAM_IN_PROGRESS:
+                Log.d(DEBUG, String.valueOf(percentComplete));
+                break;
+            case Progress.PROCESS_INPUT_STREAM_SUCCESS:
+                break;
+        }
+    }
+}
---
+++ Application/src/main/java/com/example/android/networkconnect/MainActivity.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.networkconnect;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+/**
+ * Sample Activity demonstrating how to connect to the network and fetch raw
+ * HTML. It uses a Fragment that encapsulates the network operations on an AsyncTask.
+ *
+ * This sample uses a TextView to display output.
+ */
+public class MainActivity extends FragmentActivity  {
+
+    private Button mButtonToDownLoadActivity;
+    private Button mButtonToGetStreamingActivity;
+    private View.OnClickListener clkLstnr =new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.button_DL:
+                    Intent mIntent = new Intent(getApplicationContext(), DownloadAndViewActivity.class);
+                    startActivity(mIntent);
+                    break;
+                case R.id.button_STREAM:
+                    break;
+            }
+
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        mButtonToDownLoadActivity = (Button) findViewById(R.id.button_DL);
+        mButtonToDownLoadActivity.setOnClickListener(clkLstnr);
+        mButtonToGetStreamingActivity = (Button) findViewById(R.id.button_STREAM);
+        mButtonToGetStreamingActivity.setOnClickListener(clkLstnr);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+
+}
